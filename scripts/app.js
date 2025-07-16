8dc5b3f4931b75291462f2ac6a96207fa05a2129
Muhannadjz <muhannadzowayed05@gmail.com>
2025-07-16 11:33:17 +0300

    card data and added 18 logo images

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+const cardData = [
+    {id: 1, img: '668.png'},
+    {id: 2, img: 'alabraaj.png'},
+    {id: 3, img: 'bacon.png'},
+    {id: 4, img: 'baskinrobbins.png'},
+    {id: 5, img: 'cinecafe.png'},
+    {id: 6, img: 'cinnabon.png'}
+    {id: 7, img: 'dose.png'}
+    {id: 8, img: 'haji.png'}
+    {id: 9, img: 'hcr.png'}
+    {id: 10, img: 'jan.png'}
+    {id: 11, img: 'jasmislogo.png'}
+    {id: 12, img: 'lilou.png'}
+    {id: 13, img: 'samona.png'}
+    {id: 14, img: 'shay.png'}
+    {id: 15, img: 'trentren.png'}
+    {id: 16, img: 'vapiano.png'}
+    {id: 17, img: 'villamamas.png'}
+    {id: 18, img: 'zaytzaytoon.png'}
+]
